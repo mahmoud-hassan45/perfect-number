fad03e6e4baaed7a7e38f07f38655394ba255808
mahmoud-hassan45 <116374410+mahmoud-hassan45@users.noreply.github.com>
2022-12-25 17:45:06 +0200

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿// See https://aka.ms/new-console-template for more information
+int i, j, sum;
+Console.WriteLine("enter start number");
+int start = int.Parse(Console.ReadLine());
+Console.WriteLine("enter  end");
+int end = int.Parse(Console.ReadLine());
+for(i=start;i<=end;i++)
+{
+    sum = 0;
+    for (j = 1; j < i; j++)
+    {
+        if (i % j == 0) 
+        {
+            sum += j;
+        }
+    }
+        if (sum == i)
+            {
+            Console.WriteLine(i);
+    }
+        
+}   
+
+
+
+
+    
